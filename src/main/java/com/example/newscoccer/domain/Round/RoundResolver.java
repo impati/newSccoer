a40c7cdf1992e3de553645f955f59ea308dec55e
impati <impati0716@icloud.com>
2022-11-12 23:27:40 +0900

    feat : RoundResolver 오버로딩 및 기능 완성

--- src/main/java/com/example/newscoccer/domain/Round/RoundResolver.java
+++ src/main/java/com/example/newscoccer/domain/Round/RoundResolver.java
@@ -1,5 +1,6 @@
 package com.example.newscoccer.domain.Round;
 
+import com.example.newscoccer.exception.NotFoundRoundFunction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,29 @@ import java.util.List;
 @RequiredArgsConstructor
 public class RoundResolver {
     private final List<RoundFunction> roundFunctionList ;
+    private final RoundUtils roundUtils;
+    public <T> T roundFunction(RoundDto arg ,  Class feature,  Class<T> returnType){
+        for(int i = 0;i<roundFunctionList.size();i++){
+            RoundFunction function = roundFunctionList.get(i);
+            if(feature.isInstance(function)){
+                if(function.supports(roundUtils.getRound(arg.getRoundId()))){
+                    return function.feature(arg,returnType);
+                }
+            }
+        }
+        throw new NotFoundRoundFunction("찾고자하는 라운드 기능이 없습니다.");
+    }
+    public void roundFunction(RoundDto arg ,  Class feature){
+        for(int i = 0;i<roundFunctionList.size();i++){
+            RoundFunction function = roundFunctionList.get(i);
+            if(feature.isInstance(function)){
 
-
+                if(function.supports(roundUtils.getRound(arg.getRoundId()))){
+                    function.feature(arg);
+                    return ;
+                }
+            }
+        }
+        throw new NotFoundRoundFunction("찾고자하는 라운드 기능이 없습니다.");
+    }
 }
